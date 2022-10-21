24b231f91a2dd05f8e03d406994fe36a4106868d
Charles <charlesgunawan32@gmail.com>
2022-10-21 13:16:05 +0700

    pertama

--- Chapter4/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers');
-
-// auth (regis dan login)
-router.post('/register', controller.auth.register);
-router.post('/login', controller.auth.login);
-
-module.exports = router;
